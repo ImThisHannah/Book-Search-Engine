2f25e7e009b08115357c14589b88093c4a853c31
Hannah Myers <hannahmyers510@gmail.com>
2025-02-05 02:05:50 -0500

    example deleted

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://hannahmyers510:<db_password>@cluster0.rbmqk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 const db = mongoose.connection;
 
