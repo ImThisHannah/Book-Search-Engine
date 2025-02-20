24cb0683d88ed7714103562e3cc014d038cc2b72
Hannah Myers <hannahmyers510@gmail.com>
2025-02-20 18:42:36 -0500

    modified:   connection.ts

--- Develop/server/src/config/connection.ts
+++ Develop/server/src/config/connection.ts
@@ -3,8 +3,32 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://hannahmyers510:<db_password>@cluster0.rbmqk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+// Get the MongoDB URI from environment variables
+const mongoURI = process.env.MONGODB_URI;
+
+if (!mongoURI) {
+  console.error('MongoDB URI is not defined in the .env file');
+  process.exit(1); 
+}
+
+mongoose.connect(mongoURI)
+  .then(() => {
+    console.log('Successfully connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1); 
+  });
 
 const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('MongoDB connection established');
+});
+
+// Log when there are connection errors
+db.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
 
 export default db;
